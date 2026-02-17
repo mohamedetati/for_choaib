e3cac5eb8ed8c4bce2ae00026451344c60fe260b
mohamedetati <coursarabic04@gmail.com>
2026-02-17 16:19:44 +0000

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('viewPoem').addEventListener('click', () => {
-    const poemText = document.getElementById('poemInput').value.trim();
-    if(poemText){
-        localStorage.setItem('poemText', poemText);
-        window.location.href = 'poem.html';
-    } else {
-        alert("من فضلك أدخل القصيدة أولاً!");
-    }
-});
